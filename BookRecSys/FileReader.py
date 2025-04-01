425d8405fe3741c74abb5b91003dd4710fa26466
Ronnie Thai <Ronniethai08@gmail.com>
2025-03-31 21:35:26 -0400

    commit

--- BookRecSys/FileReader.py
+++ BookRecSys/FileReader.py
@@ -8,99 +8,104 @@ import matplotlib.pyplot as plt
 import os
 import nltk
 
-#Changes the directory path 
-os.chdir(r'C:\Users\ronni\Python Project\BookRecSys')
+#Make this class a function
+def get_cleaned_data():
+    
+    #Changes the directory path 
+    os.chdir(r'C:\Users\ronni\Python Project\BookRecSys')
 
-#Allows panda to read the data from the csv Files
-books = pd.read_csv('DataSets/Books.csv')
-ratings = pd.read_csv('DataSets/Ratings.csv')
-users = pd.read_csv('DataSets/Users.csv')
+    #Allows panda to read the data from the csv Files
+    books = pd.read_csv('DataSets/Books.csv')
+    ratings = pd.read_csv('DataSets/Ratings.csv')
+    users = pd.read_csv('DataSets/Users.csv')
 
-print(books.head())
-print(ratings.head())
-print(users.head())
+    print(books.head())
+    print(ratings.head())
+    print(users.head())
 
-print(books.shape)
-print(ratings.shape)
-print(users.shape)
+    print(books.shape)
+    print(ratings.shape)
+    print(users.shape)
 
-# Checks for missing values
-print("\nMissing values in books DataFrame:")
-print(books.isna().sum())
+    # Checks for missing values
+    print("\nMissing values in books DataFrame:")
+    print(books.isna().sum())
 
-# Checks for rows where the author is missing 
-print("\nRows with missing Book-Author:")
-missing_authors = books[books['Book-Author'].isna()]
-print(missing_authors)
+    # Checks for rows where the author is missing 
+    print("\nRows with missing Book-Author:")
+    missing_authors = books[books['Book-Author'].isna()]
+    print(missing_authors)
 
-#Locate the missing values of books
-books.loc[118033, 'Book-Title']
-books.loc[187689, 'Book-Title']
+    #Locate the missing values of books
+    books.loc[118033, 'Book-Title']
+    books.loc[187689, 'Book-Title']
 
-#Add the missing author to those books
-books.loc[118033, 'Book-Author'] = 'Downes, Larissa Anne'
+    #Add the missing author to those books
+    books.loc[118033, 'Book-Author'] = 'Downes, Larissa Anne'
 
-#Get rid of index 187689 due to no author 
-books = books.drop(index=187689).reset_index(drop=True)
+    #Get rid of index 187689 due to no author 
+    books = books.drop(index=187689).reset_index(drop=True)
 
-#recheck If there are any missing values 
-print(books.isna().sum())
+    #recheck If there are any missing values 
+    print(books.isna().sum())
 
-#Get the user data
-print("\nUsers Data:")
-print(users.isna().sum())
+    #Get the user data
+    print("\nUsers Data:")
+    print(users.isna().sum())
 
-#get the users age column 
-users.drop(columns=['Age'], inplace=True)
-print("\n", ratings.isnull().sum())
+    #get the users age column 
+    users.drop(columns=['Age'], inplace=True)
+    print("\n", ratings.isnull().sum())
 
-#check for duplicate values 
-print(books.duplicated().sum())
-print(users.duplicated().sum())
-print(ratings.duplicated().sum())
+    #check for duplicate values 
+    print(books.duplicated().sum())
+    print(users.duplicated().sum())
+    print(ratings.duplicated().sum())
 
-#Perform EDA
-print(books.head)
-print(books.dtypes)
+    #Perform EDA
+    print(books.head)
+    print(books.dtypes)
 
-#>> FIX 67 - 74 IF ANY PROBLEM OCCURS 
-#Fix incorrect year of publication
-#books.loc[209538]
-#print(books.loc[209538, 'Book-Title'])
-#books.loc[209538, 'Year-Of-Publication']
+    #>> FIX 67 - 74 IF ANY PROBLEM OCCURS 
+    #Fix incorrect year of publication
+    #books.loc[209538]
+    #print(books.loc[209538, 'Book-Title'])
+    #books.loc[209538, 'Year-Of-Publication']
 
-#books.loc[220731]
+    #books.loc[220731]
 
-#books.loc[221678]
+    #books.loc[221678]
 
-invalid_years = books[pd.to_numeric(books['Year-Of-Publication'], errors='coerce').isna()]
-print("Invalid Year-Of-Publication entries:")
-print(invalid_years[['Book-Title', 'Year-Of-Publication']])
+    invalid_years = books[pd.to_numeric(books['Year-Of-Publication'], errors='coerce').isna()]
+    print("Invalid Year-Of-Publication entries:")
+    print(invalid_years[['Book-Title', 'Year-Of-Publication']])
 
-books['Year-Of-Publication'] = pd.to_numeric(books['Year-Of-Publication'], errors='coerce').fillna(0).astype('int64')
+    books['Year-Of-Publication'] = pd.to_numeric(books['Year-Of-Publication'], errors='coerce').fillna(0).astype('int64')
 
-print("\nAfter fixing, unique years are:")
-print(books['Year-Of-Publication'].unique())
+    print("\nAfter fixing, unique years are:")
+    print(books['Year-Of-Publication'].unique())
 
-books['Year-Of-Publication'].value_counts().sort_index(ascending=False).loc[:20]
+    books['Year-Of-Publication'].value_counts().sort_index(ascending=False).loc[:20]
 
-#Check to see if any publish dates are incorrect
-print(books[books['Year-Of-Publication']>2021][['Book-Title', 'Year-Of-Publication', 'Publisher', 'Book-Author']])
+    #Check to see if any publish dates are incorrect
+    print(books[books['Year-Of-Publication']>2021][['Book-Title', 'Year-Of-Publication', 'Publisher', 'Book-Author']])
 
-#>>>> Used to display full title of books to get publish date
-# print(books.loc[37487, 'Book-Title'])
-# print(books.loc[55676, 'Book-Title'])
-# print(books.loc[80264, 'Book-Title'])
-# print(books.loc[118294, 'Book-Title'])
-# print(books.loc[192992, 'Book-Title'])
+    #>>>> Used to display full title of books to get publish date
+    # print(books.loc[37487, 'Book-Title'])
+    # print(books.loc[55676, 'Book-Title'])
+    # print(books.loc[80264, 'Book-Title'])
+    # print(books.loc[118294, 'Book-Title'])
+    # print(books.loc[192992, 'Book-Title'])
 
-books.loc[[37487, 55676, 78168, 80264, 97826, 116053, 118294, 192992, 228172, 240168, 246841, 255408, 260973]
-          , 'Year-Of-Publication'] = [1991, 2005, 2003, 2003, 2001, 1981, 1995, 2023, 1987, 1996, 1925, 1937, 1991]
-books.loc[37487, 'Book-Author'] = 'Bruce Coville'
+    books.loc[[37487, 55676, 78168, 80264, 97826, 116053, 118294, 192992, 228172, 240168, 246841, 255408, 260973]
+            , 'Year-Of-Publication'] = [1991, 2005, 2003, 2003, 2001, 1981, 1995, 2023, 1987, 1996, 1925, 1937, 1991]
+    books.loc[37487, 'Book-Author'] = 'Bruce Coville'
 
-print(books['Year-Of-Publication'].value_counts().sort_index().loc[:20])
-books['Year-Of-Publication'].value_counts().sort_index(ascending=False).loc[:20]
+    print(books['Year-Of-Publication'].value_counts().sort_index().loc[:20])
+    books['Year-Of-Publication'].value_counts().sort_index(ascending=False).loc[:20]
 
-#Check the index of num. of books published each year 
-books_years_rational = books[books['Year-Of-Publication']!=0]['Year-Of-Publication'].value_counts().sort_index(ascending=False).loc[:20]
-print(books_years_rational)
+    #Check the index of num. of books published each year 
+    books_years_rational = books[books['Year-Of-Publication']!=0]['Year-Of-Publication'].value_counts().sort_index(ascending=False).loc[:20]
+    print(books_years_rational)
+
+    return books, ratings, users, books_years_rational
--- BookRecSys/graph.py
+++ BookRecSys/graph.py
@@ -1,9 +1,26 @@
 #Create graphs to show data 
 #What data : ?? 
-import pandas as pd
-import seaborn as sns
-import numpy as np
-import warnings
+from FileReader import get_cleaned_data
 import matplotlib.pyplot as plt
-import os
-import nltk
+
+#grab the data from FileReader.py and import it into graph.py
+books, ratings, users, books_year_rational = get_cleaned_data()
+
+#Create graph
+plt.figure(figsize=(16, 9))
+
+#Filter the graph
+filtered_books = books_year_rational[books_year_rational.index >= 1900]
+
+bars = plt.bar(x=filtered_books.index, height=filtered_books.values)
+
+for bar in bars:
+    height = bar.get_height()
+    plt.text(bar.get_x() + bar.get_width() / 2, height, '{:.0f}'.format(height), ha='center', va='bottom', rotation=45, fontsize=8)
+
+plt.xticks(rotation=45, fontsize=10)
+plt.title("Number of Books Published Yer Year")
+plt.xlabel("Year")
+plt.ylabel("Number of Books")
+plt.tight_layout()
+plt.show()
